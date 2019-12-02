e2a78b4e637ba57fb06fd70cefd8e3c381eaea80
Renzo Carara <renzo.carara@libero.it>
2019-12-02 19:36:47 +0100

    implementato bonus

--- main.js
+++ main.js
@@ -3,17 +3,28 @@ $(document).ready(function() {
 
     // catturo click per invio messaggio
     $('#send-input').click(function() {
-        console.log($('#r-input-bar input').val());
         var messageToSend = $('#r-input-bar input').val();
 
-        // clono un elemento template e rimuovo la classe template
-        var HTMLnewElement = $('.template').clone().removeClass('template');
+        if (messageToSend) {
+            // procedo solo se l'utente ha inserito del testo nel campo di input
 
-        // valorizzo l'elemento HTML da aggiungere nel contenitore conversazioni con il testo introdotto dall'utente
-        $(HTMLnewElement).text(messageToSend);
+            // clono un elemento template e rimuovo la classe template
+            var HTMLnewElement = $('.template').clone().removeClass('template');
 
-        // faccio una append del nuovo elemento all'interno del contenitore delle conversazioni
-        $('#r-conversation').append(HTMLnewElement);
+            // valorizzo l'elemento HTML da aggiungere nel contenitore conversazioni con il testo introdotto dall'utente
+            $(HTMLnewElement).text(messageToSend);
+
+            // faccio una append del nuovo elemento all'interno del contenitore delle conversazioni
+            $('#r-conversation').append(HTMLnewElement);
+
+        }
     });
+
+
+    // catturo click nel campo input
+    $('#text-input').click(function() {
+        // cambio icona alla destra del campo di input, rimuovendo e aggiungendo classi di Fontawesome
+        $('#send-input .fa-microphone').addClass('fa-arrow-alt-circle-right').removeClass('fa-microphone');
+    })
 
 });
