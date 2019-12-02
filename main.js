9b63fb0221acf918b0d362410743a8841f3b4416
Renzo Carara <renzo.carara@libero.it>
2019-12-02 19:19:03 +0100

    gestito il contenitore conversazioni con display:flex

--- main.js
+++ main.js
@@ -8,20 +8,11 @@ $(document).ready(function() {
 
         // clono un elemento template e rimuovo la classe template
         var HTMLnewElement = $('.template').clone().removeClass('template');
-        console.log("prima html: ", HTMLnewElement.html());
-        console.log("prima text: ", HTMLnewElement.text());
 
         // valorizzo l'elemento HTML da aggiungere nel contenitore conversazioni con il testo introdotto dall'utente
-        // $($(HTMLnewElement).html()).text(messageToSend);
-        $(HTMLnewElement).children().text(messageToSend);
+        $(HTMLnewElement).text(messageToSend);
 
-        // <div class="template"><span class="msg-wrapper msg-mine">template</span></div>
-        // '.msg-wrapper').text(messageToSend);
-
-        console.log("dopo html: ", HTMLnewElement.html());
-        console.log("dopo text: ", HTMLnewElement.text());
-
-        // faccio una append del messaggio che l'utente ha inviato all'interno del contenitore delle conversazioni
+        // faccio una append del nuovo elemento all'interno del contenitore delle conversazioni
         $('#r-conversation').append(HTMLnewElement);
     });
 
