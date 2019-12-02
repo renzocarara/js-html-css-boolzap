c7f0d5afb2bc9a608c3c20175a169b9c7f0f7d8f
Renzo Carara <renzo.carara@libero.it>
2019-12-02 16:44:45 +0100

    definito un tag input per l'invio messaggio

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
 $(document).ready(function() {
 
+
+    // catturo click per invio messaggio
+    $('#send-input').click(function() {
+        // console.log($('#r-input-bar input').attr('value'));
+        console.log($('#r-input-bar input').val());
+
+    })
+
 });
